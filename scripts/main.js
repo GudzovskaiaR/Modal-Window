4fb7554448b8d2f3c339fc7d59b8c6b1aac57e51
GudzovskaiaR <100199861+GudzovskaiaR@users.noreply.github.com>
2022-08-11 11:11:38 +0300

    created popup

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+const popupButtons = document.querySelectorAll('.open');
+const timeout = 800;
+
+if (popupButtons.length>0){
+    popupButtons.forEach((popupButton)=>{
+        popupButton.addEventListener('click',function (e){
+            const popupName = popupButton.getAttribute('id').replace('#','')
+            const currentPopup = document.getElementById(popupName)
+            popupOpen(currentPopup)
+            e.preventDefault();
+        })
+        
+    })
+}
+function popupOpen(currentPopup) {
+	
+		
+    // const popupActive = document.querySelector('.popup.open');
+    // console.log(popupActive)
+		 currentPopup.classList.add('open');
+         const popupActive = document.querySelector('.popup.open');
+         
+		currentPopup.addEventListener("click", function (e) {
+			if (!e.target.closest('.popup__content')) {
+				popupClose(e.target.closest('.popup'));
+			}
+		});
+	}
+    function popupClose(popupActive) {
+       
+            popupActive.classList.remove('open');
+            console.log(popupActive)
+        }
+   
+
+
+
+
+
